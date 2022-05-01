0c326b8ffd5159480cce9d568928160b699ecd8e
Sanchit Gupta <93199726+sanchitg21@users.noreply.github.com>
2022-05-02 01:21:59 +0530

    Update hits.py

--- hits.py
+++ hits.py
@@ -1,74 +1,77 @@
 import networkx as nx
 import numpy as np
 
-G = nx.read_gpickle('web_graph.gpickle')
+web_graph=nx.read_gpickle("web_graph.gpickle")
+print(web_graph)
 
-print(G)
-
-# read all the queries
+#read all the queries
 query = input('Enter your query:')
 query = query.split()
 
-authority_matrix = np.zeros(100)
+arr = np.zeros(100)
 
-for i in range(0, 100):
-    node_index = i
-    doc = G.nodes[node_index]['page_content']
-    arr0 = doc.split()
+for i in range(0,100) :
+    document = web_graph.nodes[i]['page_content']
+    temp=document.split()
 
-# create an array arr0 for storing all values
     for q in query:
-        if q in arr0:
-            authority_matrix[i] = 1
+        if q in temp:
+            arr[i]=1
             break
 
-# create adjacency_matrix adjacency_matrix
-adjacency_matrix = nx.to_numpy_array(G)
-authority_matrix = np.array(authority_matrix)
-# hub_matrix=authority_matrix*adjacency_matrix
-hub_matrix = np.dot(authority_matrix, adjacency_matrix)
-# hub_transpose=authority_matrix * (adjacency_matrix)T
-hub_transpose = np.dot(authority_matrix, adjacency_matrix.T)
-# arr4=authority_matrix+hub_matrix+hub_transpose
-arr4 = np.add(authority_matrix, np.add(hub_matrix, hub_transpose))
-# create the baseset
-arr5 = []
+#create adjacency matrix arr1
+adj=nx.to_numpy_array(web_graph)
 
-for i in range(0, 100):
-    if(arr4[i] != 0):
-        # add i to a vector arr5
-        arr5.append(i)
+arr=np.array(arr)
 
-# create a subgraph using arr5
-subgraph = nx.subgraph(G, arr5)
-arr8 = nx.to_numpy_array(subgraph)
+#Getting Hub score
+hub=np.dot(arr,adj)
 
-hubscore = np.ones(len(arr5))
-authscore = np.ones(len(arr5))
-hubscore = hubscore/sum(hubscore)
-authscore = authscore/sum(authscore)
+#Getting authority score
+authority=np.dot(arr,adj.T)
 
+#Finding which all pages involved in hub-authority relationship for the given query and are to be a part of the sub-graph
+arr=arr+hub+authority
 
-count = 0
-while(count <= 1000):
-    hubscore = np.dot(arr8, authscore)
-    authscore = np.dot(arr8.T, hubscore)
-    hubscore = hubscore/sum(hubscore)
-    authscore = authscore/sum(authscore)
-    count = count+1
+#create the baseset
+connection=[]
 
-arr5 = list(subgraph.nodes)
+for i in range (0,100):
+    if(arr[i]!=0):
+        #add i to a vector arr5
+        connection.append(i)
+
+#create a subgraph using arr5
+subgraph=nx.subgraph(web_graph, connection)
+result=nx.to_numpy_array(subgraph)
+
+length = len(connection)
+hubvector=np.ones(length)
+authvector=np.ones(length)
+hubvector=hubvector/length
+authvector=authvector/length
+
+count=0
+# Setting no. of iterations
+while(count<=1000):
+    hubvector=np.dot(result,authvector)
+    authvector=np.dot(result.T,hubvector)
+    hubvector=hubvector/sum(hubvector)
+    authvector=authvector/sum(authvector)
+    count=count+1
+
+result=list(subgraph.nodes)
 
 print("\nNode\tHub Values:")
-index = 0
-for x in hubscore:
-    print(arr5[index], end="\t")
-    print(x, end="\n")
-    index = index+1
+step=0
+for value in hubvector:
+    print(result[step],end="\t")
+    print(value,end="\n")
+    step=step+1
 
 print("\nNode\tAuthority Values:")
-index = 0
-for x in authscore:
-    print(arr5[index], end="\t")
-    print(x, end="\n")
-    index = index+1
+step=0
+for value in authvector:
+    print(result[step],end="\t")
+    print(value,end="\n")
+    step=step+1
