27296422c296adc978fb343410c291fd11720bf9
Rahul Jauhari <76107601+rahuljauhari3@users.noreply.github.com>
2022-05-01 19:35:15 +0530

    HITS Algorithm

--- hits.py
+++ hits.py
@@ -43,32 +43,32 @@ for i in range(0, 99):
 subgraph = nx.subgraph(G, arr5)
 arr8 = nx.to_numpy_array(subgraph)
 
-hubvector = np.ones(len(arr5))
-authvector = np.ones(len(arr5))
-hubvector = hubvector/sum(hubvector)
-authvector = authvector/sum(authvector)
+hubscore = np.ones(len(arr5))
+authscore = np.ones(len(arr5))
+hubscore = hubscore/sum(hubscore)
+authscore = authscore/sum(authscore)
 
 
 count = 0
 while(count <= 1000):
-    hubvector = np.dot(arr8, authvector)
-    authvector = np.dot(arr8.T, hubvector)
-    hubvector = hubvector/sum(hubvector)
-    authvector = authvector/sum(authvector)
+    hubscore = np.dot(arr8, authscore)
+    authscore = np.dot(arr8.T, hubscore)
+    hubscore = hubscore/sum(hubscore)
+    authscore = authscore/sum(authscore)
     count = count+1
 
 arr5 = list(subgraph.nodes)
 
 print("\nNode\tHub Values:")
 index = 0
-for x in hubvector:
+for x in hubscore:
     print(arr5[index], end="\t")
     print(x, end="\n")
     index = index+1
 
 print("\nNode\tAuthority Values:")
 index = 0
-for x in authvector:
+for x in authscore:
     print(arr5[index], end="\t")
     print(x, end="\n")
     index = index+1
